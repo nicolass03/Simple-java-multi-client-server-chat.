332d6fbf3e56820399c69b9c700fca2d38b20948
Nicolas Sanabria Ortiz <nicolas.sanabria@correo.icesi.edu.co>
2019-08-10 10:58:42 -0700

    Chat ready, need to add Cesar's encryption.

---
+++ src/client/Client.java
@@ -0,0 +1,68 @@
+package client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+
+public class Client {
+
+	private static int port = 5555;
+	
+	public static void main(String args[]) throws Exception{ 
+        Scanner scn = new Scanner(System.in); 
+                    
+        // establish the connection 
+        Socket s = new Socket("localhost", port); 
+          
+        // obtaining input and out streams 
+        DataInputStream dis = new DataInputStream(s.getInputStream()); 
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
+  
+        // sendMessage thread 
+        Thread sendMessage = new Thread(new Runnable()  
+        { 
+            @Override
+            public void run() { 
+                while (true) { 
+  
+                    // read the message to deliver. 
+                    String msg = scn.nextLine(); 
+                      
+                    try { 
+                        // write on the output stream 
+                        dos.writeUTF(msg); 
+                    } catch (IOException e) { 
+                        e.printStackTrace(); 
+                    } 
+                } 
+            } 
+        }); 
+          
+        // readMessage thread 
+        Thread readMessage = new Thread(new Runnable()  
+        { 
+            @Override
+            public void run() { 
+  
+                while (true) { 
+                    try { 
+                        // read the message sent to this client 
+                        String msg = dis.readUTF(); 
+                        System.out.println(msg); 
+                    } catch (IOException e) { 
+  
+                        e.printStackTrace(); 
+                    } 
+                } 
+            } 
+        }); 
+  
+        sendMessage.start(); 
+        readMessage.start(); 
+  
+    } 
+} 
+
